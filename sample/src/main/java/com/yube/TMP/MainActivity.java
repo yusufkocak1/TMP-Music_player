e45bbbd4ca5440fcc21732e6f994894551f8f2b3
yusufkocak1 <yusufkocak.53@yandex.com>
2017-11-30 12:23:54 +0300

    aaa

--- sample/src/main/java/com/yube/TMP/MainActivity.java
+++ sample/src/main/java/com/yube/TMP/MainActivity.java
@@ -2,24 +2,30 @@ package com.yube.TMP;
 
 import android.app.Activity;
 import android.app.Dialog;
-
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.os.EnvironmentCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SimpleItemAnimator;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
-
+import android.widget.Toolbar;
+import android.support.design.widget.TabLayout;
 import com.example.jean.jcplayer.JcAudio;
 import com.example.jean.jcplayer.JcPlayerView;
-
 import com.example.jean.jcplayer.JcStatus;
 import com.yube.TMP.Contact.PlayListContact;
 import com.yube.TMP.process.Database;
@@ -42,11 +48,39 @@ public class MainActivity extends Activity
     private Database db = new Database(this);
     private ImageButton scanbtn;
     private ArrayList<JcAudio> jcAudios;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+
+    private int[] tabIcons = {
+            R.drawable.play,
+            R.drawable.music_list,
+            R.drawable.youtube
+    };
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //------------fragment-------------
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+
+        //------------------fragment end------
+
+
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         player = (JcPlayerView) findViewById(R.id.jcplayer);
@@ -138,6 +172,73 @@ public class MainActivity extends Activity
 
 
     }
+
+
+
+    private void setupTabIcons() {
+
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("ONE");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.play, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setText("TWO");
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.music_list, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setText("THREE");
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.youtube, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+    }
+
+    public void deneme(){
+
+
+        Button btn;
+        btn.add
+
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new OneFragment(), "ONE");
+        adapter.addFrag(new TwoFragment(), "TWO");
+        adapter.addFrag(new ThreeFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
+
 
     protected void adapterSetup() {
         audioAdapter = new AudioAdapter(player.getMyPlaylist());
@@ -334,7 +435,7 @@ public class MainActivity extends Activity
     class alert {
         private Dialog dialog;
 
-        public void showalert(Activity activity) {
+        public void showalert(MainActivity activity) {
             dialog = new Dialog(activity);
 
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
---
+++ sample/src/main/java/com/yube/TMP/onFragment.java
@@ -0,0 +1,31 @@
+package com.yube.TMP;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yusuf on 28.11.2017.
+ */
+
+
+public class onFragment extends Fragment   {
+
+    public onFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
